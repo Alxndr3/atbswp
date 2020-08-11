70461e9899d5e59b8606f5f7bf797923786f5017
alexandre <alxndr3@gmail.com>
2020-08-10 22:16:53 -0300

    Commit 1

--- rename_dates_2.py
+++ rename_dates_2.py
@@ -5,12 +5,33 @@ import re
 import shutil
 
 
-us_style_date = re.compile(r'([0-1][0-9])?-([0-3][0-9])?-(\d){4}')
-for file in os.listdir(os.chdir('./delicious')):
-   usd = us_style_date.search(file)
-   if usd:
-      usd_list = usd.group().split('-')
-      file_list = file.split(usd.group())
-      esd = usd_list[1] + '-' + usd_list[0] + '-' + usd_list[2]
-      new_name = file_list[0] + esd + file_list[1]
-      shutil.move(file, new_name)
+# Create a regex that matches files with the American date format.
+date_pattern = re.compile(r"""
+    ^(.*?)              # all text befor the date
+    ((0|1)?\d)-         # one or two digits for the month
+    ((0|1|2|3)?\d)-     # one or two digits for the day
+    ((19|20)\d\d)       # four digits for the year
+    (.*?)$              # all text after the date
+    """, re.VERBOSE)
+# Loop over the files in the working directory.
+for amer_filename in os.listdir('./delicious'):
+    mo = date_pattern.search(amer_filename)
+# Skip files without a date.
+    if mo == None:
+        continue
+# Get the different parts of the filename.
+    before_part = mo.group(1)
+    month_part = mo.group(2)
+    day_part = mo.group(4)
+    year_part = mo.group(6)
+    after_part = mo.group(8)
+# Form the European-style filename.
+    euro_filename = before_part + day_part + '-' + month_part + '-' + year_part + after_part
+# Get the full, absolute file paths.
+    abs_working_dir = os.path.abspath('./delicious')
+    amer_filename = os.path.join(abs_working_dir, amer_filename)
+    euro_filename = os.path.join(abs_working_dir, euro_filename)
+# Rename the files.
+    print('Renaming "%s" to "%s"...' % (amer_filename, euro_filename))
+    shutil.move(amer_filename, euro_filename) # uncomment after testing
+
