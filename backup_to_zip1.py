a89f9fecbd49d25f6fc7c633326d3e545481bdc0
Alexandre <alxndr3@gmail.com>
2020-08-12 00:08:59 -0300

    Rename

--- backup_to_zip1.py
+++ backup_to_zip1.py
@@ -1,5 +1,5 @@
 #! python3
-# backup_to_zip.py - Copies an entire folder and its contents into a zip file whose filename increments
+# backup_to_zip1.py - Copies an entire folder and its contents into a zip file whose filename increments
 import re
 import os
 import zipfile
---
+++ backup_to_zip2.py
@@ -0,0 +1,11 @@
+#! python3
+# backup_to_zip1.py - Copies an entire folder and its contents into a zip file whose filename increments
+import re
+import os
+import zipfile
+
+
+def backupToZip(folder):
+
+
+
