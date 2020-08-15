0831fbdd51c7cd064cfa58bda1ca6da8893cd5fb
Alexandre <alxndr3@gmail.com>
2020-08-15 15:20:08 -0300

    Added option to copy folder tree or parent folder only.

--- copy_by_extension.py
+++ copy_by_extension.py
@@ -3,7 +3,7 @@ import re
 import shutil
 
 
-def copy_by_extension(from_folder, to_folder, extension):
+def copy_by_extension(from_folder, to_folder, extension, tree):
     """
     copy_by_extension.py - searches a folder or folder tree and copies files with a certain extension to another
     chosen folder.
@@ -14,20 +14,32 @@ def copy_by_extension(from_folder, to_folder, extension):
     :return: List of copied files.
     """
     extension_ = re.compile(r'(.*?)(%s)' %(extension))
-    for folder, sub_folder, file_name in os.walk(from_folder):
-        try:
-            for files in file_name:
-                if extension_.search(files):
-                    print('Copying %s/%s' %(folder, files))
-                    print('To %s/%s' %(to_folder, files))
-                    shutil.copy(folder + '/' + files, to_folder)
-        except shutil.SameFileError:
-            print('Same file found: %s' % (file_name[0]))
-            break
+    if tree.upper() == 'Y':
+        for folder, sub_folder, file_name in os.walk(from_folder):
+            try:
+                for files in file_name:
+                    if extension_.search(files):
+                        print('Copying %s/%s' %(folder, files))
+                        print('To %s/%s' %(to_folder, files))
+                        shutil.copy(folder + '/' + files, to_folder)
+            except shutil.SameFileError:
+                print('Same file found: %s' % (file_name[0]))
+                break
+    elif tree.upper() == 'N'.upper():
+        for file in os.listdir(from_folder):
+            try:
+                if extension_.search(file):
+                    print('Copying %s/%s' %(from_folder, file))
+                    print('To %s/%s' %(to_folder, file))
+                    shutil.copy(from_folder + '/' + file, to_folder)
+            except shutil.SameFileError:
+                print('Same file found: %s' % (file))
+                break
 
 
 copy_by_extension('/home/alexandre/PycharmProjects/atbswp/delicious',
                   '/home/alexandre/PycharmProjects/atbswp/bacon',
-                  '.jpg')
+                  '.txt',
+                  'y')
 #copy_by_extension('/home/alexandre/Downloads', 'delicious','.pdf', 'n')
 
