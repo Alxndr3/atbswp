131b74654e4654515700b774ca3c5768ba95c1b1
Alexandre Cardoso <alxndr3@gmail.com>
2020-11-24 10:32:07 -0300

    Practice Project Chapter 15

--- pdf_paranoia.py
+++ pdf_paranoia.py
@@ -4,28 +4,32 @@
 import os
 import PyPDF2
 import sys
+from time import sleep
 
 if len(sys.argv) < 3:
     print('Insert a password for pdf encryption and the path to directory to be searched')
 else:
-    print(sys.argv[1])
-    print(sys.argv[2])
-    try:
-        for folder_name, subfolders, filenames in os.walk(sys.argv[2]):
-            print(sys.argv[2])
-            for filename in filenames:
-                if filename.endswith('.pdf'):
-                    print(filename)
-                    with open(filename, 'rb') as pdf_file:
-                        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-                        pdf_writer = PyPDF2.PdfFileWriter()
-                        for page_num in range(pdf_reader.numPages):
-                            pdf_writer.addPage(pdf_reader.getPage(page_num))
+    for folder_name, subfolders, filenames in os.walk(sys.argv[2]):
+        for filename in filenames:
+            if filename.endswith('.pdf'):
+                with open(folder_name + '/' + filename, 'rb') as pdf_file:
+                    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+                    pdf_writer = PyPDF2.PdfFileWriter()
+                    for page_num in range(pdf_reader.numPages):
+                        pdf_writer.addPage(pdf_reader.getPage(page_num))
+                    pdf_writer.encrypt(sys.argv[1])
+                    with open(folder_name + '/' + filename + '_encrypted.pdf', 'wb') as result_pdf:
+                        pdf_writer.write(result_pdf)
 
-                        pdf_writer.encrypt(sys.argv[1])
-                        with open(filename + '_encrypted.pdf', 'wb') as result_pdf:
-                            pdf_writer.write(result_pdf)
 
-    except FileNotFoundError:
-        print(filename + ' is already encrypted.')
-        sys.exit()
+                try:
+                    with open(filename + '_encrypted.pdf', 'rb') as pdf_file_r:
+                        pdf_reader_r = PyPDF2.PdfFileReader(pdf_file_r)
+                        if pdf_reader_r.isEncrypted:
+                            pdf_reader_r.decrypt(sys.argv[1])
+                            pdf_obj = pdf_reader_r.getPage(0)
+                except:
+                    print('It has failed!')
+                else:
+                    #os.remove(folder_name + '/' + filename)
+                    print(f"Deleting...{folder_name + '/' + filename}")
